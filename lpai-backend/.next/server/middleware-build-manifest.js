6988a2e19191e811616c05fe5dcfa80619238bb2
Michael Dean <BlackSheap311ca@gmail.com>
2025-05-23 13:25:25 -0600

    Update vercel.json and workspace configs for Vercel monorepo build

--- lpai-backend/.next/server/middleware-build-manifest.js
+++ lpai-backend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,30 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:[],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-cedddee8ffdbc8f1.js","static/chunks/framework-8aadf6ff92dab596.js","static/chunks/main-14937f675a7bb3ad.js","static/chunks/pages/_app-1c5015f947cb846f.js"],"/_error":["static/chunks/webpack-cedddee8ffdbc8f1.js","static/chunks/framework-8aadf6ff92dab596.js","static/chunks/main-14937f675a7bb3ad.js","static/chunks/pages/_error-97a89d23f1d0121c.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
